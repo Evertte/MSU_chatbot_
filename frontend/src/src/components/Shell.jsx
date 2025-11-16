9f97eac9d6ab2e897616b2777e9ff2e9ad7467bf
Evertte Berkyaw <148358628+Evertte@users.noreply.github.com>
2025-11-15 23:25:23 -0600

    Delete frontend/src/src directory

--- frontend/src/src/components/Shell.jsx
+++
@@ -1,8 +0,0 @@
-// src/components/shell.jsx
-export default function Shell({ children }) {
-  return (
-    <div className="fixed inset-0">{/* no bg here */}
-      <div className="flex h-dvh w-full bg-cream">{children}</div>
-    </div>
-  );
-}
